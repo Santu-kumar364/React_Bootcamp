fb1d6eaeebf3f1c9619d1d7250a461adabcd0c67
Santu-kumar364 <santukumar7619@gmail.com>
2025-03-10 23:39:55 +0530

    add this night update

--- src/Contents.js
+++ src/Contents.js
@@ -1,32 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Contents = () => {
+    const [getname, setName] = useState("santu");
+    const [count, setCount] = useState(0);
+
+
     const handleNameChanges = () => {
         const names = ["santu", "Ajeet", "Anju"];
-        const instantChange = Math.floor(Math.random() * 3);
-        return names[instantChange];
+        const Changes = Math.floor(Math.random() * 3);
+        setName(names[Changes]);
     };
 
     const handleClicks = ()=> {
-        console.log("you're have clicked");
+        console.log(count);
     }
 
-    const handleClick2 = (name)=> {
-        console.log(`${name} has clicked`);
-    }
+    // const handleClick2 = (name)=> {
+    //     console.log(`${name} has clicked`);
+    // }
 
   return (
     <main>
       <p onDoubleClick={handleClicks}>
-        Hello {handleNameChanges()}
+        Hello {getname}
       </p>
+      <button onClick={handleNameChanges}>Change Name</button>
       <button onClick={handleClicks}>click it</button>
-      <button onClick={() => handleClick2("santu")}>click it-2</button>
+      {/* <button onClick={() => handleClick2("santu")}>click it-2</button> */}
     </main>
   )
 }
 
 export default Contents
+
+
+
+
+
+
+
 
 
 
