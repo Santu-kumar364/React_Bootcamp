3c6f8bc8541cf5dc6b4ca90ce54df9b1b27a12d1
Santu-kumar364 <santukumar7619@gmail.com>
2025-03-11 16:11:51 +0530

    learnt about list and key

--- src/Contents.js
+++ src/Contents.js
@@ -1,45 +1,114 @@
-import React, { useState } from 'react'
+// Learn Event & useState
+
+// import React, { useState } from 'react'
+
+// const Contents = () => {
+//     const [getname, setName] = useState("santu");
+//     const [count, setCount] = useState(0);
+
+//     const handleNameChanges = () => {
+//         const names = ["santu", "Ajeet", "Anju"];
+//         const Changes = Math.floor(Math.random() * 3);
+//         setName(names[Changes]);
+//     };
+
+//     const handleClicks = ()=> {
+//       setCount((prevCount) => prevCount + 1);
+//       console.log(count);
+
+//     }
+
+//     // const handleClick2 = (name)=> {
+//     //     console.log(`${name} has clicked`);
+//     // }
+
+//   return (
+//     <main>
+//       <p onDoubleClick={handleClicks}>
+//         Hello {getname}
+//       </p>
+//       <button onClick={handleNameChanges}>Change Name</button>
+//       <button onClick={handleClicks}>click it</button>
+//       {/* <button onClick={() => handleClick2("santu")}>click it-2</button> */}
+//     </main>
+//   )
+// }
+
+// export default Contents
+
+// useEffect
+
+import React, { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Contents = () => {
-    const [getname, setName] = useState("santu");
-    const [count, setCount] = useState(0);
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "One half pound bag of Cocca Covered Almonds Unsalted",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
 
 
-    const handleNameChanges = () => {
-        const names = ["santu", "Ajeet", "Anju"];
-        const Changes = Math.floor(Math.random() * 3);
-        setName(names[Changes]);
-    };
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item);
+    setItems(listItems);
+    localStorage.setItem("shoppingList", JSON.stringify(listItems));
+  };
 
-    const handleClicks = ()=> {
-        console.log(count);
-    }
-
-    // const handleClick2 = (name)=> {
-    //     console.log(`${name} has clicked`);
-    // }
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("shoppingList", JSON.stringify(listItems));
+  }
 
   return (
     <main>
-      <p onDoubleClick={handleClicks}>
-        Hello {getname}
-      </p>
-      <button onClick={handleNameChanges}>Change Name</button>
-      <button onClick={handleClicks}>click it</button>
-      {/* <button onClick={() => handleClick2("santu")}>click it-2</button> */}
+        {items.length ? (
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            <input
+              type="checkbox"
+              onChange={() => {
+                handleCheck(item.id);
+              }}
+              checked={item.checked}
+            />
+            <label
+              style={(item.checked) ? { textDecoration: "line-through" } : null}
+              onDoubleClick={() => handleCheck(item.id)}
+            >
+            {item.item}
+            </label>
+            <FaTrashAlt 
+            onClick={() => handleDelete(item.id)}
+            role="button" 
+            tabIndex="0" 
+            />
+          </li>
+        ))}
+      </ul>
+      ) : (
+        <p style={{marginTop: '2rem'}}> Your list is empty.</p>
+
+      )}
     </main>
-  )
-}
+  );
+};
 
-export default Contents
-
-
-
-
-
-
-
-
-
+export default Contents;
 
 
